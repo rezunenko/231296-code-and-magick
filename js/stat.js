44f333e949f84ba96f40447f55c1dbc5155eed71
rezunenko <tamerlan88111@yandex.ru>
2017-11-19 02:46:30 +0300

    add paper object and show it when the gage finished

---
+++ js/stat.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var Paper = function (x, y, width, height, ctx) {
+  var _x = x || 0;
+  var _y = y || 0;
+  var _width = width || 0;
+  var _height = height || 0;
+  var _ctx;
+  var _border = {};
+  if (!ctx) {
+    throw Error('Not valid CTX param');
+  } else {
+    _ctx = ctx;
+  }
+
+  function renderBorder() {
+    var borderX = _x - _border.width;
+    var borderY = _y - _border.width;
+    var borderWidth = _width + 2 * _border.width;
+    var borderHeight = _height + 2 * _border.width;
+    var grd = _ctx.createLinearGradient(0, 0, 0, _height);
+    grd.addColorStop(0, 'lightgreen');
+    grd.addColorStop(1, 'blue');
+
+    _ctx.fillStyle = grd;
+    _ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+    _ctx.shadowOffsetX = 10;
+    _ctx.shadowOffsetY = 10;
+    _ctx.fillRect(borderX, borderY, borderWidth, borderHeight);
+    _ctx.shadowOffsetX = 0;
+    _ctx.shadowOffsetY = 0;
+  }
+
+  return {
+    setBorder: function (borderWidth, color) {
+      _border.width = borderWidth;
+      _border.color = color;
+    },
+    render: function () {
+      if (_border.width) {
+        renderBorder(_ctx);
+      }
+
+      _ctx.fillStyle = '#fff';
+      _ctx.fillRect(_x, _y, _width, _height);
+      _ctx.font = '16px PT Mono';
+      _ctx.fillStyle = '#000';
+      _ctx.fillText('Ура вы победили!', _x + 20, _y + 30);
+      _ctx.fillText('Список результатов:', _x + 20, _y + 50);
+    }
+  };
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  var paper = new Paper(100, 10, 420, 270, ctx);
+  paper.setBorder(5);
+  paper.render(ctx);
+};
