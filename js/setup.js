1881a593e97dd41a7cccc7e06eaa639d9a0bc8ee
rezunenko <tamerlan88111@yandex.ru>
2017-11-25 00:38:09 +0300

    render wizard list

--- js/setup.js
+++ js/setup.js
@@ -20,17 +20,42 @@ function getRandomArrayItem(arr) {
   return arr[getRandomValue(arr.length - 1)];
 }
 
-function getCharacter(count) {
-  var characters = [];
+function getWizard(count) {
+  var wizards = [];
   count = count || 0;
 
   for (var i = 0; i < count; i++) {
-    characters.push({
+    wizards.push({
       name: getRandomArrayItem(FIRST_NAMES) + ' ' + getRandomArrayItem(LAST_NAMES),
       coatColor: getRandomArrayItem(COAT_COLORS),
       eyesColor: getRandomArrayItem(EYES_COLORS)
     });
   }
 
-  return characters;
+  return wizards;
 }
+
+function renderWizard(wizard, template) {
+  template = template.cloneNode(true);
+  template.querySelector('.setup-similar-label').textContent = wizard.name;
+  template.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  template.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return template;
+}
+
+function renderWizardList() {
+  var WIZARD_COUNT = 4;
+  var wizards = getWizard(WIZARD_COUNT);
+  var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < WIZARD_COUNT; i++) {
+    fragment.appendChild(renderWizard(wizards[i], wizardTemplate));
+  }
+
+  document.querySelector('.setup-similar-list').appendChild(fragment);
+  document.querySelector('.setup-similar').classList.remove('hidden');
+}
+
+renderWizardList();
